bd8f370ade10d3a79bb2fdc43851d965310ce22b
Tim Viney <t.c.viney@gmail.com>
2024-06-23 19:20:36 +0100

    make solve func

--- Python/lambda_function.py
+++ Python/lambda_function.py
@@ -1,7 +1,28 @@
 import json
+import solver
+from sudoku import Sudoku
 
 def lambda_handler(event, context):
+    # Parse the request body
+    body = json.loads(event['body'])
+    
+    method = body.get('method', [])
+
+    if method == 'solveMatrix': solve_matrix(body)
+    else:
+        return {
+            'statusCode': 405,
+            'body': json.dumps('Method Not Allowed')
+        }
+
+def solve_matrix(body):
+    matrix = body.get('matrix', [[]])
+
+    sudoku = Sudoku(matrix)
+    solver.solve(sudoku)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Python!!!')
+        'body': json.dumps(sudoku.matrix)
     }
+
